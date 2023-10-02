744b29b6e18e3a29ab4195690b68ae643a12e6a9
curtarnolds <afayedormawuli@gmail.com>
2023-10-02 19:07:42 +0000

    5. Not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """A sample flask app"""
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from os import getenv
 from api.v1.views import app_views
@@ -8,6 +8,11 @@ from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 @app.teardown_appcontext
