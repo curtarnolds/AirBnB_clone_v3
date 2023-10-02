e9d90e39db48956405e963dadba89c1e6dbf9637
curtarnolds <afayedormawuli@gmail.com>
2023-10-02 17:22:19 +0000

    3. Status of your API

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 """A simple views package"""
+from api.v1.views.index import *  # noqa
 from flask import Blueprint
 
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-
-from api.v1.views.index import *  # noqa
