60d424ce915f161db41ef7bd4a1705a5670fc6d8
curtarnolds <afayedormawuli@gmail.com>
2023-10-03 12:57:24 +0000

    Sync to sandbox

--- main_8.py
+++ main_8.py
@@ -10,4 +10,4 @@ if __name__ == "__main__":
     r = requests.post("http://0.0.0.0:5050/api/v1/amenities/", data=json.dumps(
         {'fake_name': "Fridge"}), headers={'Content-Type': "application/json"})
     print(r.status_code)
-    print(r.json())
+    print(r.text)
