3db4ddaaf7c939ea4f9c48ee3137a7ad11049925
curtarnolds <afayedormawuli@gmail.com>
2023-10-03 12:39:54 +0000

    Sync to sandbox

--- main_0_check.py
+++ main_0_check.py
@@ -5,7 +5,7 @@ import json  # noqa
 import requests
 
 if __name__ == "__main__":
-    r = requests.get("http://0.0.0.0:5000/api/v1/stats")
+    r = requests.get("http://0.0.0.0:5050/api/v1/stats")
     r_j = r.json()
     print(r_j.get("amenities", 0))
     print(r_j.get("cities", 0))
