7df847c6f9d35fc87e5c3bf00b63f488e0e54cfa
curtarnolds <afayedormawuli@gmail.com>
2023-10-03 23:47:12 +0000

    Add check for place in list returned from Place

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -14,7 +14,7 @@ def get_places_by_city_id(city_id):
     """Retrieve list of places of a city"""
     city_obj = storage.get(City, city_id)
     if city_obj:
-        places = [place.to_dict() for place in city_obj.places]
+        places = [place.to_dict() for place in city_obj.places if place]
         return jsonify(places)
     else:
         abort(404)
